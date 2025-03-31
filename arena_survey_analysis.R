65e991fe3f77a1783cc8fbb388c330e4ec08df54
Lauri <lauri.vesa@gmail.com>
2025-03-31 09:41:04 +0300

    Fix basunit result table 2

--- arena_survey_analysis.R
+++ arena_survey_analysis.R
@@ -788,12 +788,12 @@ arenaAnalytics <- function( dimension_list_arg, server_report_step ) {
                                   .names = "{.col}.{.fn}"),
                           item_count = n() )
       
-      base_unit.results[[i]]$weight[ is.na(base_unit.results[[i]]$weight)]   <- 0
-      base_unit.results[[i]]$item_count[ base_unit.results[[i]]$weight == 0] <- 0 
-      
       # join results with the clone of base unit
       df_base_unit <- df_base_unit %>%
         dplyr::left_join( base_unit.results[[i]], by = base_UUID_)
+      
+      base_unit$weight[ is.na(base_unit$weight)]   <- 0
+      base_unit$item_count[ base_unit$weight == 0] <- 0 
       
       
       ## PART 3. compute sum of per hectare results at the cluster level for each result variable
